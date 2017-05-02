36ca4955754300a912c30870d58e7955f9dd1b37
Gustavo Andrades <gu.andrades.ga@gmail.com>
2017-05-02 13:32:03 -0300

    Quase pronto

--- TesteJava/src/testejava/Calc.java
+++ TesteJava/src/testejava/Calc.java
@@ -10,88 +10,60 @@ package testejava;
  * @author Gustavo Andrades
  */
 import java.util.Scanner;
+
 public class Calc {
     public static void main (String args[]){
-        //criando um objeto c a apartir do metodo calc
-        Calc c = new Calc();
         //declarando as varíaveis
         int opcao;
-        int num1;
-        int num2;
-      Scanner input = new Scanner(System.in);  
-        System.out.println("-Escolha uma opção-");
-    System.out.println("1. Soma");  
-    System.out.println("2. Subtracao");  
-    System.out.println("3. Multiplicacao");  
-    System.out.println("4. Divisao");  
-    System.out.println("0. Sair");  
-    System.out.println("Operação: ");  
-    opcao = input.nextInt();
-while (opcao != 0) {
-    if (opcao == 1) {
-            ///???????
-            /**Que outra maneira poderia ser recebido 
-             * os numeros, sem que se repita as próximas 
-             * linhas para todas operações**/
-        Scanner input1 = new Scanner(System.in);  
-            System.out.println("Qual o primeiro numero: ");
-             num1 = input1.nextInt();
-            System.out.println("Qual o segundo numero: ");
-             num2 = input1.nextInt();
-             /***/
-                    int operacao = c.soma(num1, num2);
-                    System.out.println(operacao);
-                    break;
+        int num1 = 0;
+        int num2 = 0;
+        int result;
+        opcao = menu();
+                            
+switch (opcao) {
+    case 1:
+        result = soma(num1,num2);
+       break;
+    case 2:
+        result = subtracao(num1,num2);
+       break;
+    case 3:
+        result = divisao(num1,num2);
+        break;
+    case 4:
+        result = multiplicacao(num1,num2);
+        break;
+    case 5:
+         break;
+    case 6:
+        System.out.println("Digite uma opição valida.");
+        default;
     }
-    // e se eu mecher aqui?
-                    if (opcao == 2) {
-                      Scanner input1 = new Scanner(System.in);  
-                    System.out.println("Qual o primeiro numero: ");
-                     num1 = input1.nextInt();
-                    System.out.println("Qual o segundo numero: ");
-                     num2 = input1.nextInt();
-                            int operacao = c.subtracao(num1, num2);
-                            System.out.println(operacao);
-                    break;
-                    }
-                    if (opcao == 3) {
-                              Scanner input1 = new Scanner(System.in);  
-                            System.out.println("Qual o primeiro numero: ");
-                             num1 = input1.nextInt();
-                            System.out.println("Qual o segundo numero: ");
-                             num2 = input1.nextInt();
-                                    int operacao = c.multiplicacao(num1, num2);
-                                    System.out.println(operacao);
-                    break;
-                    }
-                    if (opcao == 4) {
-                              Scanner input1 = new Scanner(System.in);  
-                            System.out.println("Qual o primeiro numero: ");
-                             num1 = input1.nextInt();
-                            System.out.println("Qual o segundo numero: ");
-                             num2 = input1.nextInt();
-                                    int operacao = c.divisao(num1, num2);
-                                    System.out.println(operacao);
-                            break;
-                            }
-                    else{
-                                    System.out.println("????");
-                                    break;
-                                    }
-    }
-    }
+ }
         //Declaração das Classes
-	public int soma(int num1, int num2) {
+	public static int soma(int num1, int num2) {
 		return num1 + num2;
 	}
-	public int subtracao(int num1, int num2){
+	public static int subtracao(int num1, int num2){
 		return num1 - num2;
 	}
-	public int divisao(int num1,int num2){
+	public static int divisao(int num1,int num2){
 		return num1 / num2;
 	}
-	public int multiplicacao(int num1, int num2){
+	public static int multiplicacao(int num1, int num2){
 		return num1 - num2;
 	}
-        //Comentario de testes
+        public static int menu(){
+                  //Scanner input = new Scanner(System.in);  
+                Scanner in = new Scanner(System.in);                
+
+                  System.out.println("/////////Escolha uma opção\\\\\\\\\\\\");
+    System.out.println("1- Soma");  
+    System.out.println("2- Subtracao");  
+    System.out.println("3- Multiplicacao");  
+    System.out.println("4- Divisao");  
+    System.out.println("0- Sair");  
+    System.out.println("Operação: ");  
+    int opcao = in.nextInt();
+    return opcao;
 }
