c240e2fca197cb59c6f6c8b14a5080f4ea63fb40
Erasmo <erasm@10.132.84.46>
2017-05-10 20:02:32 -0300

    testando novo12

--- TesteJava/src/testejava/Calc.java
+++ TesteJava/src/testejava/Calc.java
@@ -1,4 +1,4 @@
-/*testando novo11
+/*testando novo12
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
