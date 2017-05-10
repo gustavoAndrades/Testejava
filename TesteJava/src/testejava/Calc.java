22c109fe6fdea40d0ca62b2a1cfeae3335d83c53
Gustavo Andrades <gu.andrades.ga@gmail.com>
2017-05-10 20:11:42 -0300

    teste3

--- TesteJava/src/testejava/Calc.java
+++ TesteJava/src/testejava/Calc.java
@@ -19,7 +19,7 @@ public class Calc {
         int num2 = 0;
         int result;
         opcao = menu();
-        //jj
+        //jjsasa
                             
 switch (opcao) {
     case 1:
